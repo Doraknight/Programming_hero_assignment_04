0ff2b376b868cdb5958cd3604fc6e24a0bba55a7
Selim Reza <mdselimreza.buft@gmail.com>
2022-02-05 03:31:08 +0600

    problem_solving_start

---
+++ problemSolving.js
@@ -0,0 +1,20 @@
+// Problem 1:
+/* anaToVori */
+
+function anaToVori(ana) {
+    Vori = ana / 16; /* 1 vori = 16 ana; */
+    return Vori;
+}
+myGoldMeasurement = anaToVori(43);
+console.log(myGoldMeasurement);
+
+// Problem 2:
+/* pandaCost */
+
+function pandaCost(cost) {}
+
+// Problem 3:
+/* picnicBudget */
+
+// Problem 4:
+/* oddFriend */
