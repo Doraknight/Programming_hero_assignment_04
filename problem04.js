92cf249327cbd06be3cf165a74c6067d8f5d5cbd
Selim Reza <mdselimreza.buft@gmail.com>
2022-02-05 22:09:18 +0600

    problem_04 added

--- problem04.js
+++ problem04.js
@@ -1,2 +1,18 @@
 // Problem 4:
 /* oddFriend */
+
+function oddFriend(friendsName) {
+    let odd = [];
+
+    for (const element of friendsName) {
+        odd = element;
+        if (odd.length % 2 != 0) {
+            let number = odd;
+            return number;
+        }
+    }
+}
+
+let friendsName = ["Selim", "Reza", "Jhanker", "Mahbub", "Persian", "Mir"];
+let nameOfOddFriend = oddFriend(friendsName);
+console.log(nameOfOddFriend);
--- problemSolving.js
+++ problemSolving.js
@@ -3,9 +3,12 @@
 
 function anaToVori(ana) {
     Vori = ana / 16; /* 1 vori = 16 ana; */
+    if (ana > 16) {
+        Vori = false; /* case added */
+    }
     return Vori;
 }
-myGoldMeasurement = anaToVori(43);
+myGoldMeasurement = anaToVori(15);
 console.log(myGoldMeasurement);
 
 // Problem 2:
@@ -23,6 +26,9 @@ function pandaCost(singaraQuantity, somuchaQuantity, jilapiQuantity) {
 
     const totalPrice =
         totalPriceOfSingara + totalPriceOfSomucha + totalPriceOfJilapi;
+    if (singaraQuantity == 0) {
+        totalPrice = false; /* case added */
+    }
     return totalPrice;
 }
 /* these three variable's are defined as piece; */
@@ -33,5 +39,45 @@ console.log(checkout);
 // Problem 3:
 /* picnicBudget */
 
+function picnicBudget(peopleNumber) {
+    if (peopleNumber <= 100) {
+        const totalBudget = 5000;
+        return totalBudget;
+    } else if (peopleNumber > 100 && peopleNumber <= 200) {
+        const first100people = 5000;
+        const second100people = 4000;
+        const totalpeople = first100people + second100people;
+        return totalpeople;
+    } else {
+        const first100people = 5000;
+        const second100people = 4000;
+        const restPeople = 3000;
+        const totalBudget = first100people + second100people + restPeople;
+        if (peopleNumber == 0) {
+            totalBudget = false; /* case added  */
+        }
+        return totalBudget;
+    }
+}
+
+let budgetOfPicnic = picnicBudget(200);
+console.log(budgetOfPicnic);
+
 // Problem 4:
 /* oddFriend */
+
+function oddFriend(friendsName) {
+    let odd = [];
+
+    for (const element of friendsName) {
+        odd = element;
+        if (odd.length % 2 != 0) {
+            let number = odd;
+            return number;
+        }
+    }
+}
+
+let friendsName = ["Selim", "Reza", "Jhanker", "Mahbub", "Persian", "Mir"];
+let nameOfOddFriend = oddFriend(friendsName);
+console.log(nameOfOddFriend);
